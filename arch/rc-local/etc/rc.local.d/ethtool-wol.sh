4d4aee0f50225539605ee9cf3ff4b3ba71c903cf
Reminance <872666026@qq.com>
2020-06-21 15:09:27 +0800

    add comment

--- arch/rc-local/etc/rc.local.d/ethtool-wol.sh
+++ arch/rc-local/etc/rc.local.d/ethtool-wol.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
-device=`ifconfig|grep enp|awk -F ":" 'NR==1{print $1}'`
-sudo ethtool -s $device wol g
-echo "DEVICE($device) ACTIVE WakeOnLAN DATE:" $(date +"%Y-%m-%d %H:%M:%S") >> /etc/rc.local.d/logs/eth-wol-active.log
+# 由于启动时无感知网卡是否启动 可能获取微空的网卡设备  遂使用systemd开启wol
+#device=`ifconfig|grep enp|awk -F ":" 'NR==1{print $1}'`
+#sudo ethtool -s $device wol g
+#echo "DEVICE($device) ACTIVE WakeOnLAN DATE:" $(date +"%Y-%m-%d %H:%M:%S") >> /etc/rc.local.d/logs/eth-wol-active.log
