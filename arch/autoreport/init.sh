fcb501323cb217779676b4229411cb4dc3234295
Reminance <872666026@qq.com>
2020-06-25 01:06:11 +0800

    fix

--- arch/autoreport/init.sh
+++ arch/autoreport/init.sh
@@ -5,11 +5,9 @@ if [ ! -d "data/base" ];then
   mkdir -p data/base
 fi
 curl "$1/projects/list" | ../../dev-tools/jq -r . > data/base/project.json
-if [ $? -ne 0 ]; then exit 0
-fi
+if [ $? -ne 0 ]; then exit 0; fi
 cat data/base/project.json | ../../dev-tools/jq -r '.data[]|[.id,.name]|join(",")' > data/base/project.list
 curl "$1/dict/all" | ../../dev-tools/jq -r . > data/base/dict.json
-if [ $? -ne 0 ]; then exit 0
-fi
+if [ $? -ne 0 ]; then exit 0; fi
 cat data/base/dict.json | ../../dev-tools/jq -r '.data[]|[.value,.label,.description]|join(",")' > data/base/dict.list
 
