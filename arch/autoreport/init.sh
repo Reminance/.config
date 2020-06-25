28565d5d26d1e36094b8ea38c3245496adadf0d4
Reminance <872666026@qq.com>
2020-06-25 16:24:41 +0800

    add data content to gitignore

--- arch/autoreport/init.sh
+++ arch/autoreport/init.sh
@@ -4,10 +4,10 @@ if [ ! -d "data/base" ];then
   echo -e "\033[36m==== initial base dir ====\033[0m"
   mkdir -p data/base
 fi
-curl "$1/projects/list" | ../../dev-tools/jq -r . > data/base/project.json
+curl "$1/projects/list" | jq -r . > data/base/project.json
 if [ $? -ne 0 ]; then exit 0; fi
-cat data/base/project.json | ../../dev-tools/jq -r '.data[]|[.id,.name]|join(",")' > data/base/project.list
-curl "$1/dict/all" | ../../dev-tools/jq -r . > data/base/dict.json
+cat data/base/project.json | jq -r '.data[]|[.id,.name]|join(",")' > data/base/project.list
+curl "$1/dict/all" | jq -r . > data/base/dict.json
 if [ $? -ne 0 ]; then exit 0; fi
-cat data/base/dict.json | ../../dev-tools/jq -r '.data[]|[.value,.label,.description]|join(",")' > data/base/dict.list
+cat data/base/dict.json | jq -r '.data[]|[.value,.label,.description]|join(",")' > data/base/dict.list
 
