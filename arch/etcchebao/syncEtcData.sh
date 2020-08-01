c55028b393ab77a457ee2f0c4de4d977f0a35163
Reminance <872666026@qq.com>
2020-08-01 12:35:41 +0800

    add distro judge

--- arch/etcchebao/syncEtcData.sh
+++ arch/etcchebao/syncEtcData.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
 
 # OS判断 与 预安装jq;
+source distro-judge.sh
+[[ "$DISTRO" == "CentOS" ]] && which jq || $PM install jq
+exit 0
 
 echo "同步开始, 当前时间: `date "+%Y-%m-%d %H:%M:%S"`"
 today=`date +'%Y-%m-%d'`
-if [ ! -s "etc-sync.conf" ];then echo "etc-sync.conf不存在"; exit 2; fi
+[ ! -s "etc-sync.conf" ] && echo "etc-sync.conf不存在" && exit 2
 api=$(sed '/^api=/!d;s/.*=//' etc-sync.conf)
 app_id=$(sed '/^app_id=/!d;s/.*=//' etc-sync.conf)
 app_secret=$(sed '/^app_secret=/!d;s/.*=//' etc-sync.conf)
@@ -24,22 +27,10 @@ auth_header='Bearer '$(echo access_token | base64)
 
 # 2. 获取黑名单下载url, 并下载到目录
 download() {
-    if [ -f "$data_download_path/$today/$1" ]; then echo "$1已存在"
-    else
-        [ -d $data_download_path/$today ] || echo "初始化下载路径$data_download_path/$today"; mkdir -p $data_download_path/$today
-        echo "正在清空下载路径" && rm -rf $data_download_path/$today/* && echo "开始下载$data_download_path/$today/$1, 当前时间: `date "+%Y-%m-%d %H:%M:%S"`"
-        wget -q -O $data_download_path/$today/$1 $2 && echo "下载$1成功, 当前时间: `date "+%Y-%m-%d %H:%M:%S"`"
-    fi
+    [ -d $data_download_path/$today ] || echo "初始化下载路径$data_download_path/$today"; mkdir -p $data_download_path/$today
+    echo "正在清空下载路径" && rm -rf $data_download_path/$today/* && echo "开始下载$data_download_path/$today/$1, 当前时间: `date "+%Y-%m-%d %H:%M:%S"`"
+    wget -q -O $data_download_path/$today/$1 $2 && echo "下载$1成功, 当前时间: `date "+%Y-%m-%d %H:%M:%S"`"
 }
-
-list_time_stamp=$(date +%s)
-list_result=$(curl $api/parking/black-list-all -H 'Authorization:'$auth_header -d '{"AccessToken":"'$access_token'","TimeStamp":"'$list_time_stamp'","Sign":"'$access_token'"}' -s -X POST | jq ".data")
-[ ! -n "$list_result" ] && echo "获取黑名单下载列表失败" && exit 2
-# 如果是广州的需要遍历data, 全国的不用
-download_filename=$(echo $list_result | jq ".filename" | sed 's/"//g')
-download_source=$(echo $list_result | jq ".source" | sed 's/"//g')
-[ ! -n "$download_source" ] && echo  "获取黑名单下载url失败" && exit 2
-download $download_filename $download_source
 
 # 3. 解压到指定目录
 unpack() {
@@ -54,8 +45,20 @@ unpack() {
     echo "所有文件已解压成功, json文件共计[`ls $data_unpack_path/$today/json -l |grep "^-"|wc -l`]个, 当前时间: `date "+%Y-%m-%d %H:%M:%S"`"
 }
 
-unpack $download_filename
+downloadAndUnpack() {
+    [ -f "$data_download_path/$today/$1" ] && echo "$1已存在" && return
+    download $1 $2
+    unpack $1
+}
 
+list_time_stamp=$(date +%s)
+list_result=$(curl $api/parking/black-list-all -H 'Authorization:'$auth_header -d '{"AccessToken":"'$access_token'","TimeStamp":"'$list_time_stamp'","Sign":"'$access_token'"}' -s -X POST | jq ".data")
+[ ! -n "$list_result" ] && echo "获取黑名单下载列表失败" && exit 2
+# 如果是广州的需要遍历data, 全国的不用
+download_filename=$(echo $list_result | jq ".filename" | sed 's/"//g')
+download_source=$(echo $list_result | jq ".source" | sed 's/"//g')
+[ ! -n "$download_source" ] && echo  "获取黑名单下载url失败" && exit 2
+downloadAndUnpack $download_filename $download_source
 echo "同步结束, 当前时间: `date "+%Y-%m-%d %H:%M:%S"`"
 
 
--- arch/mount.sh
+++ arch/mount.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-[[ ! -z "$1" ]] && [[ "$1" =~ mount|umount ]] || (echo "USAGE: sudo ./mount.sh mount/umount"; exit 2)
+[[ -z "$1" ]] && echo "USAGE: sudo ./mount.sh mount/umount" && exit 2
+[[ ! "$1" =~ mount|umount ]] && echo "USAGE: sudo ./mount.sh mount/umount" && exit 2
 [[ -d /run/media/xc/G ]] || echo "init mount dir"; mkdir -p /run/media/xc/G
-sleep 1
 if [[ "$1" == "mount" ]]; then
     mount /dev/sdc1 /run/media/xc/G
 elif [[ "$1" == "umount" ]]; then
