81079db52df3c813ae44e7ab041a7dcac0080011
Reminance <872666026@qq.com>
2020-08-01 21:11:02 +0800

    update

--- arch/etcchebao/addCrontab.sh
+++ arch/etcchebao/addCrontab.sh
@@ -2,8 +2,6 @@
 
 [ ! -s "etc-sync.conf" ] && echo "etc-sync.conf不存在" && exit 2
 current_path=$(cd "$(dirname $0)";pwd)
-# log_path=$(sed '/^log.path=/!d;s/.*=//' $current_path/etc-sync.conf)
-# today=`date +'%Y%m%d'`
 crontab -u $1 -l | grep syncEtcData.sh && echo "任务已存在, 请先删除任务重新添加" && exit 0
 echo "$2 mkdir -p \`sed '/^log.path=/!d;s/.*=//' $current_path/etc-sync.conf\`/\`date +\"\%Y\%m\%d\"\`;$current_path/syncEtcData.sh >> \`sed '/^log.path=/!d;s/.*=//' $current_path/etc-sync.conf\`/\`date +\"\%Y\%m\%d\"\`/\`date +\"\%Y\%m\%d\"\`.log 2>&1" >> /var/spool/cron/$1
 
