b0fd148d60b0a1273b7cf1f5edd92f64abb3b1d2
Reminance <872666026@qq.com>
2021-02-23 17:39:40 +0800

    fix amixer not working

--- i3/scripts/vol-up.sh
+++ i3/scripts/vol-up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/bin/amixer -qM set Master 5%+ umute
-#pactl set-sink-volume @DEFAULT_SINK@ +5%
+# /usr/bin/amixer -qM set Master 5%+ unmute
+pactl set-sink-volume @DEFAULT_SINK@ +5%
 # bash ~/.config/dwm/scripts/dwm-status-refresh.sh
