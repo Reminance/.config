bca5a81487aff56389f8d79fe3062e10bff2edba
Reminance <872666026@qq.com>
2021-03-04 12:07:42 +0800

    script update

--- bin/pacman-local-install.sh
+++ bin/pacman-local-install.sh
@@ -3,13 +3,19 @@
 # 对于Archlinux及类系统，对已经安装的软件进行备份是一个很好的习惯，能够在我们系统出问题或者重装一次性还原系统所需软件。
 # 首先生成软件包列表：
 
-sudo pacman -Qqen > ~/.config/arch/packages-backup/packages-repository-`date '+%Y%m%d%H%M%S'`.txt
-sudo pacman -Qqem > ~/.config/arch/packages-backup/packages-AUR-`date '+%Y%m%d%H%M%S'`.txt
+date_format_packages=~/.config/arch/packages-backup/packages-`date '+%Y%m%d%H%M%S'`.txt
+date_format_packages_aur=~/.config/arch/packages-backup/packages-aur-`date '+%Y%m%d%H%M%S'`.txt
+latest_packages=~/.config/arch/packages-backup/packages-latest.txt
+latest_packages_aur=~/.config/arch/packages-backup/packages-aur-latest.txt
+sudo pacman -Qqen > $date_format_packages
+sudo pacman -Qqem > $date_format_packages_aur
+cp $date_format_packages $latest_packages
+cp $date_format_packages_aur $latest_packages_aur
 
 # 重新安装：
-# cat ~/.config/arch/packages-backup/packages-AUR.txt | xargs yaourt -S --needed --noconfirm
-# sudo pacman --needed -S - < ~/.config/arch/packages-backup/packages-test.txt
-# cat /home/xc/.config/arch/packages-backup/packages-AUR-test.txt | xargs yay -S
+# cat ~/.config/arch/packages-backup/packages-aur-latest.txt | xargs yaourt -S --needed --noconfirm
+# sudo pacman --needed -S - < ~/.config/arch/packages-backup/packages-latest.txt
+# cat /home/xc/.config/arch/packages-backup/packages-aur-latest.txt | xargs yay -S
 
 # 清理多余软件包
 # yaourt -R `pacman -Qdqt`
