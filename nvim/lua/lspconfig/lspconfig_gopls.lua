6d8d3936c00ebe50fc6e28865856e8478aaae57c
Reminance <872666026@qq.com>
2021-03-21 23:16:38 +0800

    update

--- nvim/lua/lspconfig/lspconfig_gopls.lua
+++ nvim/lua/lspconfig/lspconfig_gopls.lua
@@ -2,16 +2,16 @@ local M = {}
 function M.setup()
     local lspconfig = require'lspconfig'
     lspconfig.gopls.setup {
-    cmd = {"gopls", "serve"},
-    settings = {
-      gopls = {
-        analyses = {
-          unusedparams = true,
+        cmd = {"gopls", "serve"},
+        settings = {
+            gopls = {
+                analyses = {
+                    unusedparams = true,
+                },
+                staticcheck = true,
+            },
         },
-        staticcheck = true,
-      },
-    },
-  }
+    }
     -- Utility servers
     local map = function(type, key, value)
         vim.api.nvim_buf_set_keymap(0,type,key,value,{noremap = true, silent = true});
@@ -40,34 +40,34 @@ function M.setup()
     map('n','<Leader>ep', '<Cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
 
     function goimports(timeoutms)
-    local context = { source = { organizeImports = true } }
-    vim.validate { context = { context, "t", true } }
+        local context = { source = { organizeImports = true } }
+        vim.validate { context = { context, "t", true } }
 
-    local params = vim.lsp.util.make_range_params()
-    params.context = context
+        local params = vim.lsp.util.make_range_params()
+        params.context = context
 
-    -- See the implementation of the textDocument/codeAction callback
-    -- (lua/vim/lsp/handler.lua) for how to do this properly.
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
-    if not result or next(result) == nil then return end
-    local actions = result[1].result
-    if not actions then return end
-    local action = actions[1]
+        -- See the implementation of the textDocument/codeAction callback
+        -- (lua/vim/lsp/handler.lua) for how to do this properly.
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+        if not result or next(result) == nil then return end
+        local actions = result[1].result
+        if not actions then return end
+        local action = actions[1]
 
-    -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
-    -- is a CodeAction, it can have either an edit, a command or both. Edits
-    -- should be executed first.
-    if action.edit or type(action.command) == "table" then
-      if action.edit then
-        vim.lsp.util.apply_workspace_edit(action.edit)
-      end
-      if type(action.command) == "table" then
-        vim.lsp.buf.execute_command(action.command)
-      end
-    else
-      vim.lsp.buf.execute_command(action)
+        -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
+        -- is a CodeAction, it can have either an edit, a command or both. Edits
+        -- should be executed first.
+        if action.edit or type(action.command) == "table" then
+            if action.edit then
+                vim.lsp.util.apply_workspace_edit(action.edit)
+            end
+            if type(action.command) == "table" then
+                vim.lsp.buf.execute_command(action.command)
+            end
+        else
+            vim.lsp.buf.execute_command(action)
+        end
     end
-  end
 end
 
 return M
